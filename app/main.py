3635ba2658ad86009db8f62c14d893e78f9b4665
mauvehed <mauvehed@attrition.org>
2022-05-04 00:13:48 -0500

    debug

--- app/main.py
+++ app/main.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
     ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    print(ip_addr)
+    print(type(ip_addr))
     html_raw = '<p><br><center><h1>Is your IP address: {} </h1></center>'
-    return html_raw.format(ip_addr[-1])
+    print(html_raw.format(ip_addr))
+    return html_raw.format(ip_addr)
