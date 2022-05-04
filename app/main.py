63240a1ca444b9635773653d394f5b44d8ab4a55
mauvehed <mauvehed@attrition.org>
2022-05-04 00:30:18 -0500

    fixed problem, made tuple, probably terrible

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
     ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-    print(ip_addr)
-    print(type(ip_addr))
     my_ip = tuple(map(str, ip_addr.split(', ')))
-    print(type(my_ip))
     html_raw = '<p><br><center><h1>Is your IP address: {} </h1></center>'
     return html_raw.format(my_ip[0])
