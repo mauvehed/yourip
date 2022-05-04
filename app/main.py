2ab56387647ae1cc71d8fdd6b93a301cc851fd5a
mauvehed <mauvehed@attrition.org>
2022-05-04 00:05:20 -0500

    only show first IP (ext?)

--- app/main.py
+++ app/main.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def home_view():
     ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     html_raw = '<p><br><center><h1>Is your IP address: {} </h1></center>'
-    return html_raw.format(ip_addr)
+    return html_raw.format(ip_addr[0])
