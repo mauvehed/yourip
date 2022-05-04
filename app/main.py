9b4185d6c72e64945d9cb56d6a5c74e14af705b4
mauvehed <mauvehed@attrition.org>
2022-05-04 00:20:55 -0500

    again?

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ def home_view():
     ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     print(ip_addr)
     print(type(ip_addr))
+    my_ip = tuple(map(str, ip_addr.split(', ')))
+    print(type(my_ip))
     html_raw = '<p><br><center><h1>Is your IP address: {} </h1></center>'
-    print(html_raw.format(ip_addr))
-    return html_raw.format(ip_addr)
+    print(html_raw.format(my_ip[0]))
+    return html_raw.format(my_ip)
