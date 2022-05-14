28809b79489bfd446d94e3a25512852b5681b8d1
mauvehed <mauvehed@attrition.org>
2022-05-13 19:40:02 -0500

    refactor(main.py): :art: Create separate get_ip() func outside home_view()
    Move the IP handling outside of home_view() and instead let it be a separate function to be called by home_view() and future routes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-    my_ip = tuple(map(str, ip_addr.split(', ')))
+    my_ip = get_ip()
     html_raw = '<p><br><center><h1>Your IP address is: {} </h1></center>'
     return html_raw.format(my_ip[0])
+
+@app.route("/json")
+def json_view():
+    my_ip = get_ip()
+    ip = {"ip" : my_ip[0]}
+    return ip
+
+@app.route("/raw")
+def raw_view():
+    my_ip = get_ip()
+    return my_ip[0]
+
+def get_ip():
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    my_ip = tuple(map(str, ip_addr.split(', ')))
+    return my_ip
