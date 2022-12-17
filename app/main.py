29b7996ae5f942dbeca46ac7af8c6388524682fc
mauvehed <mh@mvh.dev>
2022-12-17 20:55:49 +0000

    fix: properly display static image

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
+IMG_FOLDER = os.path.join('static','IMG')
+app.config('UPLOAD_FOLDER') = IMG_FOLDER
 
 @app.route("/")
 def home_view():
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Calvin.png')
     my_ip = get_ip()
-    html_raw = '<p><br><center><img src="images/Calvin.png"><br><h2> {} </h2></center>'
-    return html_raw.format(my_ip[0])
+    return render_template("index.html", home_image = full_filename, my_ip_is = my_ip[0])
+
+#    html_raw = '<p><br><center><img src="{{ home_image }}"><br><h2> {} </h2></center>'
+#    return html_raw.format(my_ip[0])
 
 @app.route("/json")
 def json_view():
