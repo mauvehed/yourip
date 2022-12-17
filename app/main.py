c7c622299b6ea7860497c47a82391d90d18b4df0
mauvehed <mh@mvh.dev>
2022-12-17 15:48:20 -0600

    fix: serve image and move to template file for html

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
 IMG_FOLDER = os.path.join('static','IMG')
 app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 
@@ -10,9 +12,6 @@ def home_view():
     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Calvin.png')
     my_ip = get_ip()
     return render_template("index.html", home_image = full_filename, my_ip_is = my_ip[0])
-
-#    html_raw = '<p><br><center><img src="{{ home_image }}"><br><h2> {} </h2></center>'
-#    return html_raw.format(my_ip[0])
 
 @app.route("/json")
 def json_view():
@@ -29,3 +28,6 @@ def get_ip():
     ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     my_ip = tuple(map(str, ip_addr.split(', ')))
     return my_ip
+
+if __name__ == "__main__":
+    app.run()
